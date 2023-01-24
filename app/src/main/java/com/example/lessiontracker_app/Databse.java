d7e776b1a66476aeb20333816b24fe091264a512
Rahil Essani <bitf19a029@pucit.edu.pk>
2023-01-25 01:43:10 +0500

    Student db set up

--- app/src/main/java/com/example/lessiontracker_app/Databse.java
+++ app/src/main/java/com/example/lessiontracker_app/Databse.java
@@ -1,4 +1,22 @@
 package com.example.lessiontracker_app;
 
-public class Databse {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Database extends SQLiteOpenHelper {
+    private static final String DB_NAME = "LessonDB";
+    private static final String TABLE_NAME = "Students";
+    public Database (Context context){super(context,DB_NAME,null,1);}
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    String create = "create table if not exists " + TABLE_NAME + "(id integer primary key autoincrement" +
+            ", name text)";
+    sqLiteDatabase.execSQL(create);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
