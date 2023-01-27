5f68dba72eb2921d0aa74cefffd6f88dd877712b
Rahil Essani <bitf19a029@pucit.edu.pk>
2023-01-27 12:05:04 +0500

    Recycler view class completed

--- app/src/main/java/com/example/lessiontracker_app/StudentRecyclerViewAdapter.java
+++ app/src/main/java/com/example/lessiontracker_app/StudentRecyclerViewAdapter.java
@@ -1,4 +1,52 @@
 package com.example.lessiontracker_app;
 
-public class StudentRecyclerViewAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class StudentRecyclerViewAdapter extends RecyclerView.Adapter<StudentRecyclerViewAdapter.MyVH> {
+    List<Student> studentsList;
+    public StudentRecyclerViewAdapter(List<Student> studentsList) {
+        this.studentsList = studentsList;
+    }
+
+    @NonNull
+    @Override
+    public StudentRecyclerViewAdapter.MyVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.single_item, parent, false);
+        return new MyVH(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StudentRecyclerViewAdapter.MyVH holder, int position) {
+        holder.data = studentsList.get(position);
+        holder.textViewStudent.setText(holder.data.getName());
+        holder.textViewCity.setText(holder.data.getCity());
+    }
+
+    @Override
+    public int getItemCount() {
+        return studentsList.size();
+    }
+
+
+    public class MyVH extends RecyclerView.ViewHolder {
+        TextView textViewStudent;
+        TextView textViewCity;
+        Student data;
+        public MyVH(@NonNull View itemView) {
+            super(itemView);
+            textViewStudent = itemView.findViewById(R.id.textViewFriendName);
+            textViewCity = itemView.findViewById(R.id.textViewCity);
+        }
+    }
 }
